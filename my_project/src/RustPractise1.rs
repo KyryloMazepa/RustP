7b5671c7c5cee5220f09a7de0abbd82c58d504e3
Kyrylo <kirillmazepa35@gmail.com>
2024-11-07 17:32:20 +0200

    Advanced traits

--- my_project/src/RustPractise1.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let x: i32 = 5; // Initialize x with a value
-    let y: i32; // Uninitialized but unused, still a warning, but doesn't cause an error
-
-    assert_eq!(x, 5); // Now x is initialized, so this works
-    println!("Success!");
-}
