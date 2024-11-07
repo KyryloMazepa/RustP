8a08d010aaf7621edc8716b36bc0fa98e2862621
Kyrylo <kirillmazepa35@gmail.com>
2024-11-07 18:04:01 +0200

    HashMap

---
+++ my_project/src/main.rs
@@ -0,0 +1,37 @@
+mod variables3;
+mod basictypes4;
+mod ownershipandborrowing5;
+mod compound_types_string;
+mod compound_types_array;
+mod compound_types_slice;
+mod compound_types_tuple;
+mod compound_types_struct;
+mod compound_types_enum;
+mod flow_control;
+mod pattern_match;
+mod associated_functions_methods;
+mod generics;
+mod const_generics;
+mod traits;
+mod trait_object;
+mod advance_traits;
+mod String11;
+mod Vector11;
+mod HashMap11;
+mod ConvertbyAs12;
+mod FromInto12;
+mod r#Others12
+mod r#panic13
+mod r#resultAnd13
+mod r#PackageandCrate14
+mod r#Module14
+mod r#Module14
+mod Useandpub14;
+mod comments_and_docs;
+mod println_and_format;
+mod debug_and_display;
+mod formating;
+
+fn main() {
+    println!("Hello, world!");
+}
